82c79f24290541ea030da8f4ec91ee5ae6d1633e
FatWrinkleZ <40575375+FatWrinkleZ@users.noreply.github.com>
2022-02-22 16:48:38 -0500

    Added alot of things
    Maze generation, file serialization, player movement, goal placer, render function, more that i cant remember

--- Dungeon Assignment/dunge.c
+++ Dungeon Assignment/dunge.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "mazeGenerator.h"
 #include <string.h>	// in case you want strlen()
 #include <math.h>	// in case you want abs()
 #ifdef WIN32
@@ -9,27 +8,41 @@
 // globals
 int u_x=1, u_y=1;	// user x,y location on grid
 int g_x=1, g_y=1;	// macguffin x,y location on grid (g for "goal")
-int max_x, max_y;	// the bounds of the floorplan, determined from input file
 FILE *fp;
+char levelName[128];
+int xSize = 0;
+int ySize = 0;
+int totalSize = 0;
+int GoalPos = 0;
+//const int* MazeWidth = 60;
+//const int* MazeHeight = 30;
 
 // declaration of function prototypes
 void save_state();
 void refresh_screen();
-void init(int, char **);
+void init(int, FILE*);
 void leave_game(char *);
+FILE* Level(int, char **);
+void CheckForMovement(int, int);
+void PlayLoop();
+void SpawnGoal();
 
 #define LINESIZ	96
 
+char *MAP;
+
 // movement controls -- feel free to redefine
-#define MOV_N	'8'		// move North/Up
-#define MOV_S	'2'		// move South/Down
-#define MOV_E	'6'		// move East/Right
-#define MOV_W	'4'		// move West/Left
-#define AMOV_N	'w'		// Alternative move North/Up
-#define AMOV_S	'x'		// Alternative move South/Down
-#define AMOV_E	'd'		// Alternative move East/Right
-#define AMOV_W	'a'		// Alternative move West/Left
+#define MOV_N	'w'		// move North/Up
+#define MOV_S	's'		// move South/Down
+#define MOV_E	'd'		// move East/Right
+#define MOV_W	'a'		// move West/Left
+#define AMOV_N	'8'		// Alternative move North/Up
+#define AMOV_S	'2'		// Alternative move South/Down
+#define AMOV_E	'6'		// Alternative move East/Right
+#define AMOV_W	'4'		// Alternative move West/Left
 #define EXIT	'.'
+
+
 
 int main(int argc, char *argv[])
 {
@@ -38,42 +51,93 @@ int main(int argc, char *argv[])
 	#else
 	printf("UNIX BASED SYSTEM DETECTED : SETTING UP WITH DEFAULTS\n");
 	#endif
-
 	char c;
+	static const int x;
+	static const int y;
+	fp = Level(argc, argv);
+	init(argc, fp);
+	SpawnGoal();
+	PlayLoop();
+	//leave_game("\nThanks for playing!\n");
+}
 
-	init(argc, argv);
-
-	while (1) {
-		refresh_screen();
-		char c;
-		#ifdef WIN_32
-		c = getch();
-		#else
-		c = getchar();
-		#endif
-		switch (c) {
-
-			// handle movement
-			// for each direction, handle its effects
-			// allow both the regular and ALT move inputs
-
-			case EXIT :	// save state and exit
-				save_state();
-				leave_game("\nGood Bye.\n");
-			break;
-
-			case '\n' : // newline -- ignore
-				// do nothing
-			break;
-
-			default:
-				printf("Error: Unrecognized input [%c]\n", c);
-			break;
-		}
-		if (g_x == u_x && g_y == u_y) {
-			leave_game("\n\n\tCongratulations! You found the MacGuffin!\n\n\n");
-		}
+void SpawnGoal(){
+	srand(time(0));
+	int x = 1+(rand()%totalSize);
+	if(MAP[x] == ']' || MAP[x] == '['){
+		SpawnGoal();
 	}
+	GoalPos = x;
+}
+
+void CheckForMovement(int dx, int dy){
+	int playerPos = (u_x + (xSize*u_y));
+	if((dx < 0 && u_x==0) || (dx > 0 && u_x == xSize)){
+		return;
+	}
+	if((dy < 0 && u_y == 0) || (dy > 0 && u_y == ySize)){
+		return;
+	}
+	char TOMOVE = MAP[playerPos + dx + (dy*xSize)];
+	switch (TOMOVE)
+	{
+	case ' ':
+			u_x+=dx;
+			u_y+=dy;
+		break;
+	
+	default:
+		break;
+	}
+}
+
+void PlayLoop(){
+	char input = 0;
+	#ifdef WIN32
+	system("cls");
+	refresh_screen();
+	input = getch();
+	#else
+	system("clear");
+	refresh_screen();
+	input = getchar();
+	#endif
+	if (g_x == u_x && g_y == u_y) {
+		leave_game("\n\n\tCongratulations! You found the MacGuffin!\n\n\n");
+	}
+
+		int dx = 0;
+		int dy = 0;
+	switch(input){
+		case EXIT :	// save state and exit
+			save_state();
+			leave_game("\nGood Bye.\n");
+		break;
+		case MOV_N:
+			dy--;
+			dx++;
+		break;
+		case MOV_E:
+			dx++;
+		break;
+		case MOV_W:
+			dx--;
+		break;
+		case MOV_S:
+			dy++;
+			dx--;
+		break;
+		case '\n' : // newline -- ignore
+			// do nothing
+		break;
+		default:
+			printf("Error: Unrecognized input [%c]\n", input);
+		break;
+	}
+	CheckForMovement(dx, dy);
+	
+
+	PlayLoop();
 }
 
 void save_state() 
@@ -84,39 +148,62 @@ void save_state()
 
 void refresh_screen()
 {
-	char buf[LINESIZ];
-	int i;
-
-	rewind(fp);
-	printf("\n");
-	fgets(buf, LINESIZ, fp);	// skip first line
-	fgets(buf, LINESIZ, fp);	// skip second line
-	for (i=0; fgets(buf, LINESIZ, fp); i++) {
-		// print the line to the screen
-		// if the player is there, overlay the '@' symbol
-		// if the magguffin is within 2 spaces of the user, print it as well
+	int playerPos = (u_x + (xSize*u_y));
+	for(int i = 0; i < totalSize; i++){
+		if(i == playerPos){
+			printf("%c", 'P');
+			//i++;
+		}else if(i == GoalPos){printf("%c", 'M');}
+		else{
+			if(MAP[i] == '\n'){
+				printf("\r");
+				printf("\n");
+			}else{
+				printf("%c", MAP[i]);
+			}
+		}
 	}
 }
 
-void init(int argc, char *argv[])
+void init(int argc, FILE* fp)
 {
-	if (argc != 2) {
+	/*if (argc != 2) {
 		printf("usage: %s <map file>\n", argv[0]);
 		exit(0);
-	}
+	}*/
 
-	fp = fopen(argv[1], "r+");
+	//fp = fopen(argv[1], "r+");
 
 	if (fp == (FILE *)NULL) {
-		printf("error: cannot open game file %s for reading\n", argv[1]);
+		printf("error: cannot open game file for reading\n");
 		exit(0);
 	}
 	if (fscanf(fp, "%d,%d", &u_x, &u_y) != 2 || fscanf(fp, "%d,%d", &g_x, &g_y) != 2) {
-		printf("error: file %s has incorrect header format\n", argv[1]);
+		printf("error: file has incorrect header format\n");
 		exit(0);
 	}
-	fprintf(stderr, "Init: Got user=[%d,%d] goal=[%d,%d]\n", u_x, u_y, g_x, g_y);
-
+	fprintf(stdout, "Init: Got user=[%d,%d] goal=[%d,%d]\n", u_x, u_y, g_x, g_y);
+	char buf[10];
+	int doneX = 0;
+	int index = 0;
+	MAP = (char*)malloc(sizeof(char));
+	while(fscanf(fp, "%c", buf) > 0){
+		if(doneX != 1){
+			xSize++;
+		}
+		if(buf[0] == '\n' && index!=0){
+			ySize++;
+			doneX = 1;
+		}
+		MAP = (char*)realloc(MAP,sizeof(char) + index*sizeof(char));
+		char buffer[2];
+		sprintf(buffer, "%c", buf[0]);
+		strcat(MAP, buffer);
+		index++;
+	}
+	totalSize = index;
+	printf("SIZE = %d (X) & %d(Y)\n", xSize, ySize);
+	printf("%s", MAP);
 	#ifdef WIN32
 	return;
 	#endif
@@ -132,4 +219,41 @@ void leave_game(char *msg)
 	#endif
 	printf("%s", msg);
 	exit(0);
+}
+
+FILE* Level(int argc, char *argv[]){
+	FILE* level;
+	int widthInt = 0;
+	int heightInt = 0;
+	printf("Would you like to play with a new maze? Or use an existing one?\n TYPE :\n 1 - For New Maze\n 2 - For Existing Maze\n");
+	int option = 0;
+	scanf("%d", &option);
+	if(option == 1){
+		char* argvs[4];
+		argvs[0] = argv[0];
+		printf("What do you want to call the level?\n");
+		scanf("%s", levelName);
+		argvs[1] = levelName;
+		char width[4];
+		printf("Type level width : ");
+		scanf("%d", &widthInt);
+		sprintf(width, "%d", widthInt);
+		argvs[2] = width;
+		char height[4];
+		printf("Type level height : ");
+		scanf("%d", &heightInt);
+		sprintf(height, "%d", heightInt);
+		argvs[3] = height;
+		return InitializeMazeGen(4, argvs);
+	}else if(option == 2){
+		printf("What level do you want to load? : ");
+		scanf("%s", levelName);
+		level = fopen(levelName, "r");
+	}else{
+		printf("ERROR : UNRECOGNIZED INPUT!\n Trying Again...\n");
+		return Level(argc, argv);
+	}
+	//fclose(level);
+	//level = fopen(levelName, "r");
+	return level;
 }
---
+++ Dungeon Assignment/mazeGenerator.h
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void CarveMaze(char *maze, int width, int height, int x, int y) {
+
+   int x1, y1;
+   int x2, y2;
+   int dx, dy;
+   int dir, count;
+   int MaggufinPosx, MaggufinPosy;
+   dir = rand() % 4;
+   count = 0;
+   while(count < 4) {
+      dx = 0; dy = 0;
+      switch(dir) {
+      case 0:  dx = 1;  break;
+      case 1:  dy = 1;  break;
+      case 2:  dx = -1; break;
+      default: dy = -1; break;
+      }
+      x1 = x + dx;
+      y1 = y + dy;
+      x2 = x1 + dx;
+      y2 = y1 + dy;
+      if(   x2 > 0 && x2 < width && y2 > 0 && y2 < height
+         && maze[y1 * width + x1] == 1 && maze[y2 * width + x2] == 1) {
+         maze[y1 * width + x1] = 0;
+         maze[y2 * width + x2] = 0;
+         x = x2; y = y2;
+         dir = rand() % 4;
+         count = 0;
+      } else {
+         dir = (dir + 1) % 4;
+         count += 1;
+      }
+   }
+
+}
+
+void GenerateMaze(char *maze, int width, int height) {
+   int x, y;
+   for(x = 0; x < width * height; x++) {
+      maze[x] = 1;
+   }
+   maze[1 * width + 1] = 0;
+   srand(time(0));
+   for(y = 1; y < height; y += 2) {
+      for(x = 1; x < width; x += 2) {
+         CarveMaze(maze, width, height, x, y);
+      }
+   }
+   maze[0 * width + 1] = 0;
+   maze[(height - 1) * width + (width - 2)] = 0;
+}
+
+FILE* SerializeMaze(char *maze, int width, int height, char* argv1){
+    FILE* file = fopen(argv1, "w");
+    //printf("Erroring Here");
+   GenerateMaze(maze , width, height);
+   int x, y;
+   fprintf(file, "002,001\n001,000\n");
+   for(y = 0; y < height; y++) {
+      for(x = 0; x < width; x++) {
+         switch(maze[y * width + x]) {
+         case 1:  fprintf(file, "[]");  break;
+         default: 
+         fprintf(file, "  ");  
+         break;
+         }
+      }
+      fprintf(file, "\n");
+      
+   }
+   fclose(file);
+   file = fopen(argv1, "r");
+   return file;
+}
+
+FILE* InitializeMazeGen(int argc, char* argv[]){
+    int width, height;
+    char *maze;
+
+    if(argc < 4){
+        printf("USAGE : %s <mazefile> <width> <height>" ,argv[0]);
+        exit(0);
+    }
+    //printf("STATS , (%s, %s)", argv[2], argv[3]);
+    sscanf(argv[2], "%d", &width);
+    sscanf(argv[3], "%d", &height);
+    maze = (char*)malloc(width * height * sizeof(char));
+    //char path[256] = "LEVELS/";
+    //strcat(path, argv[1]);
+    printf("STATS , (%d, %d)\n", width, height);
+    FILE* file = SerializeMaze(maze, width, height, argv[1]);
+    //fclose(file);
+    return file;
+}
