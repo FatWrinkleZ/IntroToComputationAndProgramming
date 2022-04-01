76f9002a19ed91360a2503720df4a3fc562cb0df
FatWrinkleZ <40575375+FatWrinkleZ@users.noreply.github.com>
2022-04-01 14:59:43 -0400

    Update assignment8.c

--- Assignment 8/assignment8.c
+++ Assignment 8/assignment8.c
@@ -5,7 +5,8 @@
 int* array;
 int length = 0;
 
-int binary_search(int index, int last, int item){
+int binary_search(int index, int last, int item){//searches by getting the midpoint between its last point and current point based 
+                                                 //on if current value is greater than or less than item of interest
     if (last >= index) { 
         int mid = index + (last - index) / 2; 
         if (array[mid] == item) 
@@ -18,7 +19,7 @@ int binary_search(int index, int last, int item){
 
 }
 
-void sort(){
+void sort(){//sorts by swapping values that are not in order
     int notSorted = 0;
     for(int i = 1; i < length; i++){
         if(array[i] < array[i-1]){
@@ -34,7 +35,7 @@ void sort(){
 
 }
 
-void printArr(){
+void printArr(){//prints array
     for( int i = 0; i < length; i++){
         printf("%d. array[%d] = %d\n", i+1, i, array[i]);
     }
@@ -42,27 +43,27 @@ void printArr(){
 
 int main(){
     printf("Please input all your values, terminated by \"END\"\n");
-    array = (int*)malloc(sizeof(int));
+    array = (int*)malloc(sizeof(int));//allocates 1 int worth of memory to array
     char buf[24];
     scanf("%s", buf);
     while(!(strcmp(buf, "END")==0)){
         array[length] = atoi(buf);
         length++;
-        array = (int*)realloc(array,sizeof(int)*(length+1));
+        array = (int*)realloc(array,sizeof(int)*(length+1));//allocates new int to array
         scanf("%s", buf);
     }
-    sort();
+    sort();//run sort function
     printf("Here is the sorted Array\n");
-    printArr();
+    printArr();//run print function
     printf("Type a value to search for : ");
     int item;
-    scanf("%d", &item);
-    int result = binary_search(0, length-1, item);
-    if(result == -1){
+    scanf("%d", &item);//intake item to search for
+    int result = binary_search(0, length-1, item);//run binary search with initial conditions
+    if(result == -1){//check if item was not in array
         printf("%d WAS NOT IN THE ARRAY\n",item);
-    }else{
+    }else{//print index that item was found in
         printf("%d WAS IN THE ARRAY AT INDEX %d\n", item, result);
     }
-    free(array);
+    free(array);//free the memory allocated to array
 
 }
